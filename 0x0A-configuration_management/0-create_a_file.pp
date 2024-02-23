53de1063e42f7740b5fc7d3d1bd8f8d646fadda5
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-24 02:24:42 +0400

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# Puppet manifest to create a file in /tmp with specific permissions
+
 file { '/tmp/school':
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => "I love Puppet\n",
 }
