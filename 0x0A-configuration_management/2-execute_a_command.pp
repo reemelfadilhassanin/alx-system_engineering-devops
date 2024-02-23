80136a7d5ae155c708ae3cd2ef2aaa3d7e08a468
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-24 02:34:17 +0400

    puppet  pkill

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Puppet manifest to kill a process named "killmenow" using pkill
+
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+}
