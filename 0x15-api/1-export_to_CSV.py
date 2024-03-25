1ceeda2015df0d624c2c707aa06db3b7d6aa5a08
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-26 01:02:57 +0400

    csv data from api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,7 +12,7 @@ base = 'https://jsonplaceholder.typicode.com/'
 
 def fetch_todo_list_progress():
     '''this defines to fetch list progress'''
-    if len(sys.argv) < 2:
+    if not len(sys.argv):
         return print('USAGE:', __file__, '<employee id>')
     eid = sys.argv[1]
     try:
@@ -25,27 +25,23 @@ def fetch_todo_list_progress():
         return print('User id not found')
     elif u_response.status_code != 200:
         return print('Error: status_code:', u_response.status_code)
-    user_d = u_response.json()
+    user = u_response.json()
 
     u_response = requests.get(base + 'todos/')
     if u_response.status_code != 200:
         return print('Error: status_code:', u_response.status_code)
-    userdos = u_response.json()
+    todos = u_response.json()
+    user_todos = [todo for todo in todos
+                  if todo.get('userId') == user.get('id')]
+    completed = [todo for todo in user_todos if todo.get('completed')]
 
-    user_todos = [todo for todo in userdos
-                  if todo.get('userId') == user_d.get('id')]
-
-    # Export to CSV
-    csv_file = f"{user_d.get('id')}.csv"
-    with open(csv_file, mode='w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow(
-            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for todo in user_todos:
-            writer.writerow([todo.get('userId'), user_d.get(
-                'name'), todo.get('completed'), todo.get('title')])
-
-    print(f"Data exported to {csv_file}")
+    with open(eid + '.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, lineterminator='\n',
+                            quoting=csv.QUOTE_ALL)
+        [writer.writerow(['{}'.format(field) for field in
+                          (todo.get('userId'), user.get('username'),
+                           todo.get('completed'), todo.get('title'))])
+         for todo in user_todos]
 
 
 if __name__ == '__main__':
