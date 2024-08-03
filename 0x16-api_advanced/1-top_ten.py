8be90302abb03f2b0b634cabbf1a925c9a78af39
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-08-03 21:58:47 +0300

    checker plzzzz

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,17 +12,17 @@ def top_ten(subreddit):
     Returns:
         None: If the subreddit is invalid or if the request fails.
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Linux:top_ten:v1.0.0'}
+    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
+    headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts[:10]:
-                print(post['data']['title'])
-        except (KeyError, TypeError):
-            print(None)
+
+    if response.status_code != 200:
+        print(None)
+        return
+    response = response.json()
+    if 'data' in response:
+        for posts in response.get('data').get('children'):
+            print(posts.get('data').get('title'))
+
     else:
         print(None)
