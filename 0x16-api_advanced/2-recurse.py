9109778d23185d9405b2fe6aafe1f71f5831a01d
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-08-03 22:12:18 +0300

    fix it now ?

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
 """A function that recursively queries the Reddit API to get all hot articles from a subreddit."""
-import pprint
+
 import requests
 
-url = 'http://reddit.com/r/{}/hot.json'
+url = 'https://www.reddit.com/r/{}/hot.json'
 
-
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """Recursively fetch all hot articles' titles from a subreddit.
 
     Args:
         subreddit (str): The name of the subreddit to query.
         hot_list (list): The list to accumulate the titles (default empty).
+        after (str): The pagination parameter to fetch the next set of results.
 
     Returns:
         list: A list of all hot article titles or None if invalid subreddit.
     """
-    headers = {'User-agent': 'Unix:0-subs:v1'}
+    headers = {'User-Agent': 'Unix:0-subs:v1'}
     params = {'limit': 100}
-    if isinstance(after, str):
-        if after != "STOP":
-            params['after'] = after
-        else:
-            return hot_list
-    response = requests.get(url.format(subreddit),
-                            headers=headers, params=params)
+    
+    if after:
+        params['after'] = after
+    
+    response = requests.get(url.format(subreddit), headers=headers, params=params, allow_redirects=False)
+    
     if response.status_code != 200:
         return None
+
     data = response.json().get('data', {})
-    after = data.get('after', 'STOP')
+    after = data.get('after')
+    
     if not after:
         after = "STOP"
-    hot_list = hot_list + [post.get('data', {}).get('title')
-                           for post in data.get('children', [])]
-    return recurse(subreddit, hot_list, after)
+    
+    hot_list.extend(post.get('data', {}).get('title') for post in data.get('children', []))
+    
+    if after == "STOP":
+        return hot_list
+    else:
+        return recurse(subreddit, hot_list, after)
