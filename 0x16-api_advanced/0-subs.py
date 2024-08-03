ec469b67ea2ad5c9921e097ee675c2d25fd66e92
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-08-03 19:33:25 +0300

    plzzz

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,16 +16,14 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers if the subreddit is valid, otherwise 0.
     """
-    url_base = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url_base = "https://api.reddit.com/r/{}/about".format(subreddit)
     headers = {'User-Agent': 'CustomClient/1.0'}
     try:
         response = requests.get(url_base, headers=headers, allow_redirects=False)
-        print(f"Status Code: {response.status_code}")  # Debug: Print status code
         if response.status_code == 200:
             response_json = response.json()
-            print(f"Response JSON: {response_json}")  # Debug: Print JSON response
             if 'data' in response_json:
                 return response_json['data'].get('subscribers', 0)
-    except Exception as e:
-        print(f"Exception: {e}")  # Debug: Print exception if any
+    except Exception:
+        pass
     return 0
