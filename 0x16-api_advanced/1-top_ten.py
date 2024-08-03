a79c6614efc4146a27a0afc0d0412062f022abd9
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-08-03 21:54:15 +0300

    top aGAIN

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API to print the titles of the top 10 posts"""
+"""A function that queries the Reddit API to print the titles of the top 10 posts."""
 
 import requests
 
 def top_ten(subreddit):
-    """print the titles of the first 10 hot posts of subreddit.
+    """Fetch and print the titles of the first 10 hot posts of a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit to query.
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     Returns:
         None: If the subreddit is invalid or if the request fails.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Linux:top_ten:v1.0.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     
@@ -20,7 +20,6 @@ def top_ten(subreddit):
         try:
             data = response.json()
             posts = data['data']['children']
-            # Print the titles of the first 10 hot posts
             for post in posts[:10]:
                 print(post['data']['title'])
         except KeyError:
