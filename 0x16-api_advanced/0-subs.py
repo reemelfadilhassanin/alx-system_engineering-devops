c9cea81fdae919c5226070e1f6ef4168e89d7e30
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-08-03 18:53:56 +0300

    urlbase

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,12 +16,12 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers if the subreddit is valid, otherwise 0.
     """
 	
-
-sub = requests.get("https://www.reddit.com/r/{}/about.json"
-                   .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-if sub.status_code >= 300:
+url_base = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+    }
+response = requests.get(url_base, headers=headers, allow_redirects=False)
+if response.status_code == 404:
     return 0
-
-return sub.json().get("data").get("subscribers")
+results = response.json().get("data")
+return results.get("subscribers")
