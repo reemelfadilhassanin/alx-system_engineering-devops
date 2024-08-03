3ff7032f2447cf32d20d15b01a613e7a734c7feb
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-08-03 18:33:22 +0300

    fix the error of sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API
+"""A function that define queries the Reddit API
 
-	Returns:
-		the number of subscribers
-	"""
+    Returns:
+        the number of subscribers
+"""
 from requests import get
 
-
-
 def number_of_subscribers(subreddit):
-	"""This to define queries the Reddit API
+    """This function define queries the Reddit API to get the number of subscribers
 
-	Args:
-		subreddit (str): The name of the subreddit to query.
+    Args:
+        subreddit (str): The name of the subreddit to query.
 
-	Returns:
-		int: The number of subscribers if is valid, otherwise 0.
-	"""
-
-	if subreddit and type(subreddit) is str:
-			subs = 0
-			url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
-			headers = {'user-agent': 'my-app/0.0.1'}
-			req = get(url, headers=headers)
-			if req.status_code == 200:
-				data = req.json()
-				subs = data.get('data', {}).get('subscribers', 0)
-			return subs
+    Returns:
+        int: The number of subscribers if the subreddit is valid, otherwise 0.
+    """
+    if subreddit and isinstance(subreddit, str):
+        subs = 0
+        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {'User-Agent': 'my-app/0.0.1'}
+        try:
+            req = get(url, headers=headers)
+            if req.status_code == 200:
+                data = req.json()
+                subs = data.get('data', {}).get('subscribers', 0)
+        except Exception:
+            pass
+        return subs
+    return 0
