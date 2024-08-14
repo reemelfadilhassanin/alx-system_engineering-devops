d581da55b7584fd1f4ccbd91ff9b34c6dd5618b2
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-08-14 21:10:20 +0300

    strace commit

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Using strace to fixes error and automated it using puppet.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
