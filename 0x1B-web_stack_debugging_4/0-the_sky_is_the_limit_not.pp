3d384a839a3f6de2272f351e4b77933918a5b6e4
reemelfadilhassanin <141276742+reemelfadilhassanin@users.noreply.github.com>
2024-08-22 06:46:52 +0300

    tray plz

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,18 +1,12 @@
 #  This Puppet manifest configures Nginx
 
 exec { 'fix--for-nginx':
-  command => '/usr/sbin/nginx -s reload',
-  path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-  notify  => Service['nginx'],
-}
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/nginx/nginx.conf',
-  notify  => Exec['fix--for-nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
